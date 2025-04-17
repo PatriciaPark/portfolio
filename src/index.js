358a75d92b983720ee506bbb804e64a3367e8e80
PATRICIA PARK <pyjee8@google.com>
2025-04-17 16:04:42 -0600

    Fix: remove basename from HashRouter

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   <React.StrictMode>
     <ThemeProvider>
       <LanguageProvider>
-        <HashRouter basename="/portfolio"> {/* ← 고정값으로 */}
+        <HashRouter>
           <App />
         </HashRouter>
       </LanguageProvider>
