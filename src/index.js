9cf8a547505de79d5c9bdd961eb6c7a5d0265ade
PATRICIA PARK <pyjee8@google.com>
2025-04-17 15:22:33 -0600

    Fix: add basename to HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import "./index.css";
 import { LanguageProvider } from "./context/LanguageContext";
 import { ThemeProvider } from "./context/ThemeContext";
+import { HashRouter } from "react-router-dom"; // 👉 추가!
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
       <LanguageProvider>
-        <App />
+        <HashRouter basename="/portfoilo">  {/* ✅ 요 줄 추가 */}
+          <App />
+        </HashRouter>
       </LanguageProvider>
     </ThemeProvider>
   </React.StrictMode>
