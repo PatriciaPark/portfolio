ce4ada9695fef22636ba6c8b0ebc6ab933a73b6e
PatriciaPark <pyj721aa@naver.com>
2025-09-04 11:06:39 -0600

    Domi update a new message UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,14 +34,14 @@ module.exports = {
     },
   },
   plugins: [],
-  safelist: [
-    {
-      pattern: /dark:shadow-\[.*\]/,
-    },
-    {
-      pattern: /dark:drop-shadow-.*/,
-    }
-  ],
+  // safelist: [
+  //   {
+  //     pattern: /dark:shadow-\[.*\]/,
+  //   },
+  //   {
+  //     pattern: /dark:drop-shadow-.*/,
+  //   }
+  // ],
 
 };
 
